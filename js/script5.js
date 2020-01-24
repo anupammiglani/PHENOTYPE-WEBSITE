1d432aa0358bbd3c272db1b3b4b42319cda50b9b
anupam <miglani.anupam14@gmail.com>
2020-01-24 12:25:15 -0800

    popup

--- js/script5.js
+++ js/script5.js
@@ -13,4 +13,17 @@ window.addEventListener("resize",setContent);
   window.addEventListener("orientationChange",setContent);
   
   setTimeout(function(){ setContent()},100);
+  
+  $('#myModal .close').on('click',function(){
+	  $('#myModal').hide();
+  });
+});
+
+$('.title-blog img.blog-image').on('click',function(event){
+	
+		
+			var nextImagePath=$(event.currentTarget).data("model-image");
+			$("#myModal .modal-image").attr("src",nextImagePath);
+			$('#myModal').show();
+	
 });
